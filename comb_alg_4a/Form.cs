8fdbbd40418211658c7c524863e6eb654bf38d69
Дмитрий Теперик <dmitrodubstep@gmail.com>
2018-12-28 22:47:45 +0300

    Добавьте файлы проекта.

---
+++ comb_alg_4a/Form.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace comb_alg_4a
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowRes(long timeSort, long timeGreed, int deltaExact, int deltaGreed, string exactAlgRes, string greedyAlg)
+        {
+
+        }
+
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void tbElems_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void tbElems_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbSum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
